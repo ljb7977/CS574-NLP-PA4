1c996d03eb27bc3907e54b56184b86ef68066365
Lee Ju Been <ljb7977@gmail.com>
2018-06-18 16:57:13 +0900

    save

--- train.py
+++ train.py
@@ -5,30 +5,31 @@ import torch
 import torch.nn as nn
 
 class Trainer():
-    def __init__(self, train_path = "data/ds_train.tsv", learningRate=0.001, epochs=10):
+    def __init__(self, train_path = "data/ds_train.tsv", learningRate=0.001):
         self.data, word2idx, pos2idx = loader.load_data_and_labels(train_path)
         args = {
             'pos_embed_dim': 256,
             'word_embed_dim': 256,
             'words_num': len(word2idx),
             'pos_num': len(pos2idx),
-            'kernel_num': 5,
-            'kernel_sizes': [64, 64, 64, 64, 64]
+            'kernel_num': 32,
+            'kernel_sizes': [1, 2, 3, 4, 5]
         }
         self.cnn_model = model.CNN_Text(args)
-        optimizer = optim.Adam(self.cnn_model.parameters(), lr=learningRate, weight_decay=0.05, amsgrad=True)
-        criterion = nn.CrossEntropyLoss()
+        self.learningRate = learningRate
 
-        for i in range(epochs):
-            print("Epoch " + str(i))
-            self.train(criterion, optimizer)
-
-    def train(self, criterion, optimizer):
+    def step(self, criterion, optimizer):
         optimizer.zero_grad()
-        logits = self.cnn_model(self.data)
-
-        loss = criterion(logits, gold)
+        labels, sentences = self.data
+        logits = self.cnn_model(sentences)
+        loss = criterion(logits, labels)
+        loss.backward()
         optimizer.step()
         print("loss: ", round(loss.item(), 2))
 
-        return
+    def train(self, epochs=5):
+        optimizer = optim.Adam(self.cnn_model.parameters(), lr=self.learningRate, weight_decay=0.05, amsgrad=True)
+        criterion = nn.CrossEntropyLoss()
+        for i in range(epochs):
+            print("Epoch " + str(i))
+            self.step(criterion, optimizer)
