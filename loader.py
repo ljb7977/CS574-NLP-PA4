crawl code fix, softmax fix

loader.py
@@ -5,12 +5,12 @@ from torch.utils.data import Dataset
 
 class TextDataset(Dataset):
     def __init__(self, filepath, word_vocab=None, pos_vocab=None):
-        self.max_sentence_length = 111
+        self.max_sentence_length = 150
         self.labels = {
-                'country': 0, 'team': 1,
-                'starring': 2, 'director': 3,
-                'child': 4, 'successor': 5
-            }
+            'country': 0, 'team': 1,
+            'starring': 2, 'director': 3,
+            'child': 4, 'successor': 5
+        }
 
         data = self.parse_line(filepath)
 
@@ -26,6 +26,7 @@ class TextDataset(Dataset):
         hannanum = Hannanum()
         lines = [line.strip() for line in open(path, "r", encoding="utf-8")]
         data = []
+        # self.max_sentence_length = 0
         for line in lines:
             sbj, obj, rel, src = line.split("\t")
 
@@ -45,6 +46,7 @@ class TextDataset(Dataset):
             # tokens = [p[0] + '/' + p[1] for p in hannanum.pos(src)]
             tokens = hannanum.morphs(src)
             print(tokens)
+            # self.max_sentence_length = max(self.max_sentence_length, len(tokens))
 
             pos1, pos2 = self.get_relative_position(tokens, e1, e2)
 

train.py
@@ -3,6 +3,7 @@ import model
 from torch import optim
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 def my_collate(batch):
@@ -12,12 +13,20 @@ def my_collate(batch):
     rel = [item['rel'] for item in batch]
     return [tokens, pos1, pos2, rel]
 
+
 class Trainer():
-    def __init__(self, train_path = "data/ds_train.tsv",
-                 test_path = "data/gold_test.tsv", learningRate=0.001):
+    def __init__(self, train_path="data/new_data.tsv",
+                 test_path="data/gold_test.tsv", learningRate=0.001):
         self.train_set = loader.TextDataset(train_path)
-        self.test_set = loader.TextDataset(test_path, word_vocab = self.train_set.word2idx,
-                                           pos_vocab = self.train_set.pos2idx)
+        self.test_set = loader.TextDataset(test_path, word_vocab=self.train_set.word2idx,
+                                           pos_vocab=self.train_set.pos2idx)
+
+        self.labels = {
+            'country': 0, 'team': 1,
+            'starring': 2, 'director': 3,
+            'child': 4, 'successor': 5
+        }
+
         args = {
             'pos_embed_dim': 256,
             'word_embed_dim': 256,
@@ -37,27 +46,30 @@ class Trainer():
                                                        shuffle=False, num_workers=4)
 
     def epoch(self, criterion, optimizer):
+        self.cnn_model.train()
         for i, data in enumerate(self.train_loader, 0):
             tokens, pos1, pos2, labels = data['tokens'], data['pos1'], data['pos2'], data['rel']
             # print(tokens, pos1, pos2, labels)
             optimizer.zero_grad()
             logits = self.cnn_model(tokens, pos1, pos2)
-            # print(logits)
-            # print(labels)
             loss = criterion(logits, labels)
             loss.backward()
             optimizer.step()
             print("step: ", i, " loss: ", round(loss.item(), 2))
 
     def train(self, epochs=5):
+
         optimizer = optim.Adam(self.cnn_model.parameters(), lr=self.learningRate, weight_decay=0.05, amsgrad=True)
         criterion = nn.CrossEntropyLoss()
         for i in range(epochs):
             print("Epoch " + str(i))
             self.epoch(criterion, optimizer)
-            self.validate(epoch=i)
+            score = self.validate(epoch=i)
+
+            torch.save(self.cnn_model, "model/" + "_".join((str(i), str(round(score, 2)), "model.model")))
 
     def validate(self, epoch):
+        self.cnn_model.eval()
         correct = 0
         total = 0
         with torch.no_grad():
@@ -68,17 +80,19 @@ class Trainer():
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
-        print("on epoch ", epoch, ", Accuracy on dev set: ", 100*correct/total)
+        print("on epoch ", epoch, ", Accuracy on dev set: ", 100 * correct / total)
+        return 100 * correct / total
 
-    def test(self):
-        correct = 0
-        total = 0
+    def test(self, load=False, model_name=None):
+        print("Test start for ", model_name)
+        self.cnn_model.eval()
+        if load:
+            self.cnn_model = torch.load("model/2/" + model_name)
+        result = []
         with torch.no_grad():
             for data in self.test_loader:
                 tokens, pos1, pos2, labels = data['tokens'], data['pos1'], data['pos2'], data['rel']
-                logits = self.cnn_model(tokens, pos1, pos2)
-                _, predicted = torch.max(logits, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+                logits = F.softmax(self.cnn_model(tokens, pos1, pos2), dim=1)
+                result.append((labels.squeeze().item(), logits.squeeze()))
 
-        print("Accuracy on test set: ", 100 * correct / total)
+        return result